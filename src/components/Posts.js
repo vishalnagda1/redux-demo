05314a8354d4784bc5f73cd06b0c2d56d3604c6b
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-10 18:27:03 +0530

    remove the posts api call from Posts component as we moved it the postAction

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,12 +5,6 @@ class Posts extends Component {
     posts: [],
   };
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((posts) => this.setState({ posts }));
-  }
-
   render() {
     const postItems = this.state.posts.map((post) => (
       <div key={post.id}>
