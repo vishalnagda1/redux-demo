b41f591806675eef2e86b5b94dcaea646d18f20f
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-10 18:16:45 +0530

    refactor the fetchPosts function in the postActions file

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,7 +1,6 @@
 import { FETCH_POSTS, NEW_POST } from "./types";
 
-export function fetchPosts() {
-  return function (dispatch) {
+export const fetchPosts = () => dispatch => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((posts) =>
