4c4387a57c2136f8538ffcd0553c728ee072187d
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-10 13:56:13 +0530

    create a Posts component and import the same in the App component

---
+++ src/components/Posts.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Posts extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => this.setState({ posts }));
+  }
+
+  render() {
+    const postItems = this.state.posts.map((post) => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
+    return (
+      <div>
+        <h1>Posts</h1>
+        {postItems}
+      </div>
+    );
+  }
+}
+
+export default Posts;
