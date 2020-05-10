c80d63e940b3a38bde5b5881ceb5eaaf3b05b002
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-10 18:51:15 +0530

    remove Posts component and used props to map posts for postItems variable

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,16 +3,12 @@ import { connect } from "react-redux";
 import { fetchPosts } from "../actions/postActions";
 
 class Posts extends Component {
-  state = {
-    posts: [],
-  };
-
   componentDidMount() {
     this.props.fetchPosts();
   }
 
   render() {
-    const postItems = this.state.posts.map((post) => (
+    const postItems = this.props.posts.map((post) => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
