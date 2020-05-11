aeb980ee0008e69f10e1509337a92816e07fe6ad
Vishal Nagda <vishalnagda1@gmail.com>
2020-05-11 15:40:22 +0530

    Updates

---
+++ precache-manifest.cbedbc69c9072389b59c0ffe6284db59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d42cd5377b47965cecb2ea5146fb696",
+    "url": "/redux-demo/index.html"
+  },
+  {
+    "revision": "4bc5af22136e9924433f",
+    "url": "/redux-demo/static/css/main.d6733f3b.chunk.css"
+  },
+  {
+    "revision": "17daf969ba5e6e0fbd88",
+    "url": "/redux-demo/static/js/2.9f6e699c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-demo/static/js/2.9f6e699c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc5af22136e9924433f",
+    "url": "/redux-demo/static/js/main.c4e0a788.chunk.js"
+  },
+  {
+    "revision": "257eac1f5d1cb6be91de",
+    "url": "/redux-demo/static/js/runtime-main.05f67abb.js"
+  }
+]);
